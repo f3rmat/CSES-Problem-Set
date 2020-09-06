c6547e8dd3bfa78b35635fc4327f9e32d27331ab
Hemang Sarkar <hemangsarkar@gmail.com>
2020-09-07 00:17:38 +0530

    Add files via upload

---
+++ subarray-sum-I.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+int main() {
+	int n;
+	long long x;
+	cin >> n >> x;
+	long long a[n];
+	for(int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	unordered_map<long long, int> mp;
+
+	long long curr_sum = 0; 
+	long long ans = 0;
+
+	for(int i = 0; i < n; i++) {
+		curr_sum += a[i];
+
+		if(curr_sum == x)
+			ans++;
+
+		if(mp[curr_sum - x] != 0) {
+			ans += mp[curr_sum - x];
+		}
+
+		mp[curr_sum]++;
+	}
+
+	cout << ans;
+
+	return 0;
+}
