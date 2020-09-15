bf8d3465db77e91b10a62258e380da65b62c38ba
Hemang Sarkar <hemangsarkar@gmail.com>
2020-09-15 18:54:38 +0530

    Update forest-queries.cpp

--- forest-queries.cpp
+++ forest-queries.cpp
@@ -11,15 +11,10 @@ int main() {
 			cin >> c; 
 			if(c == '*')
 				v[i][j] = 1;
-		}
-	}
-
-	for(int i = 1; i <= n; i++) {
-		for(int j = 1; j <= n; j++) {
 			v[i][j] += (v[i - 1][j] + v[i][j - 1] - v[i - 1][j - 1]);
 		}
 	}
-
+	
 	while(q--) {
 		cin >> y1 >> x1 >> y2 >> x2; 
 		cout << v[y2][x2] - v[y1 - 1][x2] - v[y2][x1 - 1] + v[y1 - 1][x1 - 1] << "\n";
